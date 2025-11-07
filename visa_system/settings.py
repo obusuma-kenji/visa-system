96d9a4b7d2001b8033bde1b0aefcd4b646b8b729
obusuma-kenji <kenjiarai420125@ozzio.jp>
2025-11-07 13:52:02 +0900

    Update settings.py

--- visa_system/settings.py
+++ visa_system/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-]
 
 ROOT_URLCONF = 'visa_system.urls'
 
